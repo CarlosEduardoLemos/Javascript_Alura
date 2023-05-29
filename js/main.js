642f375c523ebf7a7171197a022ef3cd00f4260b
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-29 19:06:09 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", dizOi)
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) + 1
+})
 
-function dizOi(){
-    console.log("Oi")
-}
+subtrair.addEventListener("click", (evento) => {
+    braco.value = pareInt(braco.value) - 1
+})
